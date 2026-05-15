859166f638bc941fdb1eee5a22c16ca7a40308e8
en-123-jpg <dharini.kumaravelu@gmail.com>
2026-05-15 15:41:11 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,67 @@
+from dotenv import load_dotenv
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_groq import ChatGroq
+
+load_dotenv()
+
+# Embedding model
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+# Load vector DB
+vectordb = Chroma(
+    persist_directory="chroma_db",
+    embedding_function=embeddings
+)
+
+# Groq LLM
+llm = ChatGroq(
+    model_name="llama-3.1-8b-instant"
+)
+
+print("Biofertilizer Chatbot Ready!")
+print("Type 'exit' to quit.\n")
+
+while True:
+    query = input("Ask something: ")
+
+    if query.lower() == "exit":
+        break
+
+    # Retrieve relevant chunks
+    docs = vectordb.similarity_search(query, k=2)
+
+    context = "\n".join([doc.page_content for doc in docs])
+
+    # Prompt
+    prompt = f"""
+You are a helpful AI assistant for a biofertilizer company.
+
+Use ONLY the provided context to answer.
+
+If the information is unavailable, politely say:
+"I’m not sure about that yet, but I can help with questions related to our products and services."
+Context:
+{context}
+
+Question:
+{query}
+
+Answer in a friendly and professional way.
+
+If the answer is long:
+- use bullet points
+- keep it readable
+- avoid huge paragraphs
+
+Start naturally with:
+"I can help with that."
+"""
+
+    response = llm.invoke(prompt)
+
+    print("\nAnswer:")
+    print(response.content)
+    print("\n----------------------\n")
